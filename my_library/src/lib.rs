8f16f5b64bac5e2ac45abcc08e9dd6913d61f067
Adam Colvin <adamcolv@icloud.com>
2024-07-20 08:05:49 +0100

    Replace my_library with new version
    This is the version that I developed in the FlappyDragon project. It makes use of macros to make the app setup easier, and supports main menu and game over screens.

--- my_library/src/lib.rs
+++ my_library/src/lib.rs
@@ -1,27 +1,36 @@
-//! `my_library` provides a suite of helpers to create games with Bevy.
-//!
-//! ## What's Included?
-//!
-//! `my_library` includes:
-//!
-//! * Random number generation facilities.
-//!
-//! ## Feature Flags
-//!
-//! The following feature flags are supported.
-//!
-//! ### Random Number Generation
-//!
-//! * The `locking` feature enables interior mutability inside
-//! [`RandomNumberGenerator`],
-//! allowing it to be used as a resource (Res<RandomNumberGenerator>`)
-//! rather than requiring mutability (`ResMut<RandomNumberGenerator>`)
-//! * You can control which random number generation algorithm is used by
-//! specifying *one* of:
-//!   * `xorshift` to use the XorShift algorithm.
-//!   * `pcg` to use the PCG algorithm.
-
 #![warn(missing_docs)]
+//! `my_library` provides a suite of helpers to create games with Bevy.(1)
+//! 
+//! ## What's Included?(2)
+//! 
+//! `my_library` includes:(3)
+//! 
+//! * Random number generation facilities.(4)
+//! 
+//! ## Feature Flags
+//! 
+//! The following feature flags are supported.
+//! 
+//! ### Random Number Generation
+//! 
+//! * The `locking` feature enables interior mutability inside 
+//! [`RandomNumberGenerator`], (5)
+//!   allowing it to be used as a resource (`Res<RandomNumberGenerator`) 
+//! rather than requiring mutability (`ResMut<RandomNumberGenerator>`)
+//! * You can control which random number generation algorithm is used by 
+//! specifying *one* of:
+//!    * `xorshift` to use the XorShift algorithm.
+//!    * `pcg` to use the PCG algorithm.
+
+#[cfg(not(feature = "locking"))]
+mod random;
+#[cfg(not(feature = "locking"))]
+pub use random::*;
+
+#[cfg(feature = "locking")]
+mod random_locking;
+#[cfg(feature = "locking")]
+pub use random_locking::*;
 
 /// [`RandomNumberGenerator`] wraps the `rand` crate. The `rand` crate
 /// is re-exported for your convenience.
@@ -29,19 +38,27 @@ pub mod rand {
     pub use rand::*;
 }
 
-#[cfg(not(feature = "locking"))]
-mod random;
-#[cfg(not(feature = "locking"))]
-pub use random::*;
-#[cfg(feature = "locking")]
-mod random_locking;
-#[cfg(feature = "locking")]
-pub use random_locking::*;
+mod bevy_framework;
+pub use bevy_framework::*;
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+#[macro_export]
+macro_rules! add_phase {
+  (
+    $app:expr, $type:ty, $phase:expr, //Define variables
+    start => [ $($start:expr),* ], //Describe the syntax we want in a template
+    run => [ $($run:expr),* ],
+    exit => [$($exit:expr),*]
+  ) => { //This is the code that will be used in place of the macro
+    $($app.add_systems(
+      bevy::prelude::OnEnter::<$type>($phase),
+      $start
+    );)*
+    $($app.add_systems(
+      bevy::prelude::Update, $run.run_if(in_state($phase))
+    );)*
+    $($app.add_systems(
+      bevy::prelude::OnExit::<$type>($phase),
+      $exit
+    );)*
+  }
 }
